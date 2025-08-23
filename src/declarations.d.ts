b9febbc1e91d40ec19357105abfce877bd42b191
epaker17 <eryk.pls@interia.pl>
2025-08-23 12:26:33 +0200

    chore: keep JSX entry only (remove TS duplicates)

--- src/declarations.d.ts
+++
@@ -1,5 +0,0 @@
-﻿declare module "*.json"  { const v: any; export default v; }
-declare module "*.png"   { const s: string; export default s; }
-declare module "*.jpg"   { const s: string; export default s; }
-declare module "*.jpeg"  { const s: string; export default s; }
-declare module "*.svg"   { const s: string; export default s; }
